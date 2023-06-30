e69d4b3e109c1aef483e1f22bb4b00be99c06d32
송민진 <m_6595@naver.com>
2023-06-30 12:25:22 +0900

    style: 필요 없는 코드 정리
    #6

--- src/main/java/com/tohome/bundlebundle/product/service/ProductServiceImpl.java
+++ src/main/java/com/tohome/bundlebundle/product/service/ProductServiceImpl.java
@@ -44,54 +44,28 @@ public class ProductServiceImpl implements ProductService {
         return result;
     }
 
-//    @Override
-//    public List<ProductVO> showProducts(String sortType) {
-//        List<ProductVO> result;
-//        switch (sortType) {
-//            case "best": result = findAllProductsByPopularity();
-//                break;
-//            case "newest": result = findAllProductsByLatest();
-//                break;
-//            case "price-asc": result = findAllProductsByPriceAsc();
-//                break;
-//            case "price-desc": result = findAllProductsByPriceDesc();
-//                break;
-//            case "discount":
-//                result = findAllProductsByDiscount();
-//                break;
-//            default:
-//                throw new BusinessException(ErrorCode.INVALID_INPUT_VALUE);
-//        }
-//        return result;
-//    }
-
     private List<ProductVO> findAllProductsByPopularity() {
         List<ProductVO> result = productMapper.findAllProductsByPopularity();
-        log.info(result);
         return result;
     }
 
     private List<ProductVO> findAllProductsByLatest() {
         List<ProductVO> result = productMapper.findAllProductsByLatest();
-        log.info(result);
         return result;
     }
 
     private List<ProductVO> findAllProductsByPriceAsc() {
         List<ProductVO> result = productMapper.findAllProductsPriceAsc();
-        log.info(result);
         return result;
     }
 
     private List<ProductVO> findAllProductsByPriceDesc() {
         List<ProductVO> result = productMapper.findAllProductsByPriceDesc();
-        log.info(result);
         return result;
     }
 
     private List<ProductVO> findAllProductsByDiscount() {
         List<ProductVO> result = productMapper.findAllProductsByDiscount();
-        log.info(result);
         return result;
     }
 
