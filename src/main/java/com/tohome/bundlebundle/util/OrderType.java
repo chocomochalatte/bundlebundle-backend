ea87d12890b5cd89a63fdbf7e77ece6a58691eea
송민진 <m_6595@naver.com>
2023-07-01 15:33:28 +0900

    feat: 할인률 관련 로직 추가
    #6

--- src/main/java/com/tohome/bundlebundle/util/OrderType.java
+++ src/main/java/com/tohome/bundlebundle/util/OrderType.java
@@ -9,8 +9,8 @@ public enum OrderType {
     BEST("best", "ID", "ASC"),      // REDIS 적용 필요 - 우선 ID값으로 정렬
     DATE("newest", "CREATED_AT", "DESC"),
     PRICE_ASC("price-asc", "PRICE", "ASC"),
-    PRICE_DESC("price-asc", "PRICE", "DESC"),
-    DISCOUNT_RATE("discount", "ID", "ASC");     // REDIS 적용 필요 - 우선 ID값으로 정렬
+    PRICE_DESC("price-desc", "PRICE", "DESC"),
+    DISCOUNT_RATE("discount", "D.DISCOUNT_RATE", "DESC");
 
     private String requestParam;
     private String sortingField;
