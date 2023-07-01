a0c46abc8493fdd3eb3a27412ca920271abb8f52
송민진 <m_6595@naver.com>
2023-07-01 13:03:25 +0900

    refactor: 정렬 방법에 따라 쿼리 결정하는 로직 변경
    #6

--- src/main/java/com/tohome/bundlebundle/product/mapper/ProductMapper.java
+++ src/main/java/com/tohome/bundlebundle/product/mapper/ProductMapper.java
@@ -1,5 +1,6 @@
 package com.tohome.bundlebundle.product.mapper;
 
+import com.tohome.bundlebundle.product.vo.OrderTypeVO;
 import com.tohome.bundlebundle.product.vo.ProductVO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -9,15 +10,8 @@ import java.util.Optional;
 @Mapper
 public interface ProductMapper {
 
-    List<ProductVO> findAllProductsByPopularity();
-
-    List<ProductVO> findAllProductsByLatest();
-
-    List<ProductVO> findAllProductsPriceAsc();
-
-    List<ProductVO> findAllProductsByPriceDesc();
-
-    List<ProductVO> findAllProductsByDiscount();
+    List<ProductVO> findAllProductsByOrderType(OrderTypeVO orderTypeVO);
 
     Optional<ProductVO> findProductById(Integer productId);
+
 }
--- src/main/java/com/tohome/bundlebundle/product/service/ProductServiceImpl.java
+++ src/main/java/com/tohome/bundlebundle/product/service/ProductServiceImpl.java
@@ -2,17 +2,15 @@ package com.tohome.bundlebundle.product.service;
 
 import com.tohome.bundlebundle.exception.BusinessException;
 import com.tohome.bundlebundle.exception.ErrorCode;
+import com.tohome.bundlebundle.product.vo.OrderTypeVO;
 import com.tohome.bundlebundle.product.vo.ProductVO;
 import com.tohome.bundlebundle.product.mapper.ProductMapper;
+import com.tohome.bundlebundle.util.OrderTypeConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Log4j2
 @Service
@@ -20,52 +18,17 @@ import java.util.function.Function;
 public class ProductServiceImpl implements ProductService {
     private final ProductMapper productMapper;
 
-    private static final Map<String, Function<ProductServiceImpl, List<ProductVO>>> SORT_METHODS = new HashMap<>();
-
-    static {
-        SORT_METHODS.put("best", ProductServiceImpl::findAllProductsByPopularity);
-        SORT_METHODS.put("newest", ProductServiceImpl::findAllProductsByLatest);
-        SORT_METHODS.put("price-asc", ProductServiceImpl::findAllProductsByPriceAsc);
-        SORT_METHODS.put("price-desc", ProductServiceImpl::findAllProductsByPriceDesc);
-        SORT_METHODS.put("discount", ProductServiceImpl::findAllProductsByDiscount);
-    }
-
     @Override
     public List<ProductVO> showProducts(String sortType) {
-        Function<ProductServiceImpl, List<ProductVO>> sortMethod = Optional.ofNullable(SORT_METHODS.get(sortType))
-                .orElseThrow(() -> new BusinessException(ErrorCode.INVALID_INPUT_VALUE));
-        return sortMethod.apply(this);
+        OrderTypeVO orderTypeVO = OrderTypeConverter.findOrderType(sortType);
+        List<ProductVO> result = productMapper.findAllProductsByOrderType(orderTypeVO);
+        return result;
     }
 
     @Override
     public ProductVO showProductDetail(Integer productId) {
         ProductVO result = productMapper.findProductById(productId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.PRODUCT_NOT_FOUND));
-        return result;
-    }
-
-    private List<ProductVO> findAllProductsByPopularity() {
-        List<ProductVO> result = productMapper.findAllProductsByPopularity();
-        return result;
-    }
-
-    private List<ProductVO> findAllProductsByLatest() {
-        List<ProductVO> result = productMapper.findAllProductsByLatest();
-        return result;
-    }
-
-    private List<ProductVO> findAllProductsByPriceAsc() {
-        List<ProductVO> result = productMapper.findAllProductsPriceAsc();
-        return result;
-    }
-
-    private List<ProductVO> findAllProductsByPriceDesc() {
-        List<ProductVO> result = productMapper.findAllProductsByPriceDesc();
-        return result;
-    }
-
-    private List<ProductVO> findAllProductsByDiscount() {
-        List<ProductVO> result = productMapper.findAllProductsByDiscount();
         return result;
     }
 
