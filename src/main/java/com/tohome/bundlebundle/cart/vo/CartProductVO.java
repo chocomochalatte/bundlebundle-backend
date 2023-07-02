5cbb432da08a0654b21f174d52369e980e779498
jae-ryong <frogs6225@naver.com>
2023-07-02 19:47:48 +0900

    feat: memberId 이용하기 위해 SQL문 수정
    #15

--- src/main/java/com/tohome/bundlebundle/cart/vo/CartProductVO.java
+++ src/main/java/com/tohome/bundlebundle/cart/vo/CartProductVO.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CartProductVO {
+	private int memberId;
 	private int cartId;
 	private int productId;
     private String productOrigin;
