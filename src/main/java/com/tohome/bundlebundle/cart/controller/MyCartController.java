4a16ed9eb581a1c479a7800f42016d0dad3255a4
송민진 <m_6595@naver.com>
2023-07-05 01:08:07 +0900

    feat: 모든 controller에 jwt util 적용
    #20

--- src/main/java/com/tohome/bundlebundle/cart/controller/MyCartController.java
+++ src/main/java/com/tohome/bundlebundle/cart/controller/MyCartController.java
@@ -3,17 +3,11 @@ package com.tohome.bundlebundle.cart.controller;
 import java.util.List;
 
 
+import com.tohome.bundlebundle.member.util.JwtTokenUtils;
+import com.tohome.bundlebundle.member.vo.MemberVO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.tohome.bundlebundle.cart.service.CartService;
 import com.tohome.bundlebundle.cart.vo.CartItemAddVO;
@@ -27,69 +21,65 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping(value = "api/cart")
 public class MyCartController {
-	
 	private final CartService service;
-	
-	
+	private final JwtTokenUtils jwtTokenUtils;
+
 	//개인 장바구니 조회
 	@GetMapping(value = "individual/{memberId}")
 	@ResponseBody
-	public ResponseEntity<CartVO> checkCart(@PathVariable int memberId){
-		
-		List<CartProductVO> cartProductVO = service.showItem(memberId);
-		CartVO cartvo = new CartVO();
-		cartvo.setCartProducts(cartProductVO);
-		cartvo.setCartCnt(cartProductVO.size());
-		return new ResponseEntity<CartVO>(cartvo, HttpStatus.OK);
+	public ResponseEntity<CartVO> checkCart(@RequestHeader("Authorization") String accessToken){
+		MemberVO memberVO = jwtTokenUtils.getUserFromJwtToken(accessToken);
+		List<CartProductVO> cartProductVO = service.showItem(memberVO.getId());
+		CartVO cartvo = CartVO.builder()
+							  .cartProducts(cartProductVO)
+							  .cartCnt(cartProductVO.size())
+							  .build();
+		return new ResponseEntity<>(cartvo, HttpStatus.OK);
 	}
-	
-	
+
+
 	// 개인 장바구니에 상품 조회하기 (추가 전에 중복 확인)
 	@GetMapping(value = "check/{memberId}/{productId}")
-	public ResponseEntity<CheckVO> checkItemCart(@PathVariable("memberId") int memberId, @PathVariable("productId") int productId){
-		CartItemAddVO cartItemAddVO = new CartItemAddVO();
-	    cartItemAddVO.setMemberId(memberId);
-	    cartItemAddVO.setProductId(productId);
+	public ResponseEntity<CheckVO> checkItemCart(@RequestHeader("Authorization") String accessToken, @PathVariable("productId") int productId){
+		MemberVO memberVO = jwtTokenUtils.getUserFromJwtToken(accessToken);
+		CartItemAddVO cartItemAddVO = CartItemAddVO.builder()
+												   .memberId(memberVO.getId())
+												   .productId(productId)
+												   .build();
 		CheckVO result = service.checkItemCart(cartItemAddVO);
-		return new ResponseEntity<CheckVO>(result, HttpStatus.OK);
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-	
+
 	// 개인 장바구니에 상품 추가하기
 	@PostMapping(value = "")
-	public ResponseEntity<CartItemAddVO> addItemCart(@RequestBody CartItemAddVO cartItemAddVO){
-		int result = service.addCartItem(cartItemAddVO);
-		
-		if(result>0) {
-			CartItemAddVO success = cartItemAddVO;
-			return new ResponseEntity<CartItemAddVO>(success, HttpStatus.OK);
-		}else {
-			return null;
-		}
+	public ResponseEntity<CartItemAddVO> addItemCart(@RequestBody CartItemAddVO cartItemAddVO) {
+		service.addCartItem(cartItemAddVO);
+		return new ResponseEntity<>(cartItemAddVO, HttpStatus.OK);
 	}
-	
+
 	// 개인 장바구니에 상품 삭제하기
 	@DeleteMapping(value = "{memberId}/{productId}")
-	public ResponseEntity<CheckVO> deleteCart(@PathVariable("memberId") int memberId, @PathVariable("productId") int productId){
-	    CartItemAddVO cartItemAddVO = new CartItemAddVO();
-	    cartItemAddVO.setMemberId(memberId);
-	    cartItemAddVO.setProductId(productId);
+	public ResponseEntity<CheckVO> deleteCart(@RequestHeader("Authorization") String accessToken, @PathVariable("productId") int productId){
+		MemberVO memberVO = jwtTokenUtils.getUserFromJwtToken(accessToken);
+		CartItemAddVO cartItemAddVO = CartItemAddVO.builder()
+												   .memberId(memberVO.getId())
+												   .productId(productId)
+												   .build();
 	    CheckVO result = service.deleteCartItem(cartItemAddVO);
-		return new ResponseEntity<CheckVO>(result, HttpStatus.OK);
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
-	
+
 	// 개인 장바구니에서 상품 수량 변경하기
 	@PatchMapping(value = "{memberId}/{productId}/{productCnt}")
-	public ResponseEntity<ChangeCartVO> changeProductCnt(@PathVariable("memberId") int memberId, 
-			@PathVariable("productId") int productId, @PathVariable("productCnt") int productCnt){
-		ChangeCartVO changeCartVO = new ChangeCartVO();
-		changeCartVO.setMemberId(memberId);
-		changeCartVO.setProductId(productId);
-		changeCartVO.setProductCnt(productCnt);
-		
+	public ResponseEntity<ChangeCartVO> changeProductCnt(@RequestHeader("Authorization") String accessToken,
+														 @PathVariable("productId") int productId, @PathVariable("productCnt") int productCnt){
+		MemberVO memberVO = jwtTokenUtils.getUserFromJwtToken(accessToken);
+		ChangeCartVO changeCartVO = ChangeCartVO.builder()
+								   			   .memberId(memberVO.getId())
+									 		   .productId(productId)
+											   .productCnt(productCnt)
+											   .build();
 		service.changeProductCnt(changeCartVO);
-		
-		ChangeCartVO result = changeCartVO;
-		
-		return new ResponseEntity<ChangeCartVO>(result, HttpStatus.OK);
+		return new ResponseEntity<>(changeCartVO, HttpStatus.OK);
 	}
 }
--- src/main/java/com/tohome/bundlebundle/cart/vo/CartVO.java
+++ src/main/java/com/tohome/bundlebundle/cart/vo/CartVO.java
@@ -3,11 +3,13 @@ package com.tohome.bundlebundle.cart.vo;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartVO {
