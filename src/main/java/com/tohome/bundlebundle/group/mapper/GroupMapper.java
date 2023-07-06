ea93d1609d6d6edb88192fcbb87d99934be5206b
jae-ryong <frogs6225@naver.com>
2023-07-06 23:06:55 +0900

    feat: 토큰 저장 2
    #25

--- src/main/java/com/tohome/bundlebundle/group/mapper/GroupMapper.java
+++ src/main/java/com/tohome/bundlebundle/group/mapper/GroupMapper.java
@@ -1,5 +1,6 @@
 package com.tohome.bundlebundle.group.mapper;
 
+import com.tohome.bundlebundle.group.vo.GroupMemberVO;
 import com.tohome.bundlebundle.group.vo.GroupVO;
 import org.apache.ibatis.annotations.Mapper;
 
@@ -11,4 +12,6 @@ public interface GroupMapper {
     Integer findGroupIdByGroupOwnerId(Integer memberId);
 
     Integer deleteGroupById(Integer groupId);
+
+	void insertToken(GroupMemberVO groupMemberVO);
 }
